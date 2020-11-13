cac194bfae61486a2199a83bb76ea36fa1ca59ce
Amauri Jesus <amaurisantospro@gmail.com>
2020-11-13 15:52:29 -0300

    Definição de Persistência de Dados

--- cadastro/models.py
+++ cadastro/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class DadosClientes(models.Model):
     nome = models.CharField('Nome', max_length=150, blank=True)
     telefone = models.CharField('Telefone', max_length=14, blank=True)
-    email = models.EmailField(max_length=50)
-    data_nascimento = models.DateField(max_length=10)
+    email = models.EmailField('E-mail', max_length=50)
+    data_nascimento = models.DateField('Data de Nascimento', max_length=10)
 
-    cep = models.CharField(max_length=8)
-    endereco = models.CharField(max_length=100, blank=True)
-    numero = models.CharField(max_length=6)
-    bairro = models.CharField(max_length=100, blank=True)
-    ponto_referencia = models.CharField(max_length=150)
+    cep = models.CharField('CEP', max_length=8)
+    endereco = models.CharField('Endereço', max_length=100, blank=True)
+    numero = models.CharField('Número', max_length=6)
+    bairro = models.CharField('Bairro', max_length=100, blank=True)
+    ponto_referencia = models.CharField('Ponto de Referêmcia', max_length=150)
